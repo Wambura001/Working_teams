a1d95ab86c26bc678b1f273625c30d2db5beebca
wambura <meshack.sirincha@student.moringaschool.com>
2023-09-27 01:29:35 +0900

    add all files for pushing purposes

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './header.jsx';
+import Employees from './employees.jsx';
+import Footer from './footer.jsx';
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Employees/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
